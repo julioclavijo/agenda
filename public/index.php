f735f81d4c7f24979fdcd5b945771b805f21830d
julioclavijo <julioclavijo@outlook.com>
2025-02-16 18:30:22 -0500

    update: Actualización de base_path

--- public/index.php
+++ public/index.php
@@ -1,10 +1,15 @@
 <?php
 use Slim\Factory\AppFactory;
+use Dotenv\Dotenv;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+# $app->setBasePath($_ENV['BASE_PATH']);
 $app->addBodyParsingMiddleware();
+
 
 $routes = require __DIR__ . '/../src/routes/routes.php';
 $routes($app);
